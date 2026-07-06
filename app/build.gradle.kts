2b22fad08bc35e1ff2a1117d43460115b8dc3b00
Jose Malong <jsmalong02705@liceo.edu.ph>
2026-07-06 13:52:33 +0800

    change sdk
    # Conflicts:
    #	README.md
    #	app/build.gradle.kts
    #	gradle/wrapper/gradle-wrapper.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,6 @@ android {
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 32
-        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
